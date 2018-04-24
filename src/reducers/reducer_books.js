d2c17fa41cec0f6a956bf87068bfba271acc89b4
Dylan Ellison <dylancellison@gmail.com>
2018-04-23 22:04:32 -0500

    wired up books_reducer with app

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+// standard export to make function usable anywhere in rest of project
+export default function (){
+  return [
+    {title: "Javascript: The Weird Parts"},
+    {title: "Data Structures with C++"},
+    {title: "Eloquent Javascript"},
+    {title: "Zero to One"},
+    {title: "Ryan Holiday"}
+  ]
+}
