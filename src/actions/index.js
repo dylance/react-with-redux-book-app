6542fc9abf3bd46998c8c12b862df92af2fbe5f4
Dylan Ellison <dylancellison@gmail.com>
2018-04-24 23:46:05 -0500

    action is being piped into all reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
 export function selectBook(book) {
-  console.log("A abook has been selected", book.title);
+  // SelectBook is an action creator, needs to return an action,
+  // an object with a type property.
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  }
 }
--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import { connect } from 'react-redux';
-import { selectBook}from '../actions/index';
+import { selectBook } from '../actions/index';
 import { bindActionCreators } from 'redux';
 
 
@@ -8,9 +8,14 @@ class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}</li>
-      )
-    })
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item">
+          {book.title}
+        </li>
+      );
+    });
   }
 
   render() {
