7b37e5cca7e5afc97d5595207743e19ff9d7ee42
Dylan Ellison <dylancellison@gmail.com>
2018-04-24 22:58:39 -0500

    turned book-list into a container instead of component

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,15 +1,14 @@
 import React, {Component} from 'react'
+import { connect } from 'react-redux';
 
 
-export default class BookList extends Component {
+class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
         <li key={book.title} className="list-group-item">{book.title}</li>
-
       )
     })
-
   }
 
   render() {
@@ -19,5 +18,16 @@ export default class BookList extends Component {
       </ul>
     )
   }
-
 }
+
+// this is the function that creates the link between Redux & React. the glue!
+function mapStateToProps(state){
+  // Whatever is returned from here will who up as props
+  // inside of booklist
+  return {
+    books: state.books
+
+  };
+}
+// connect takes a function and a component and produces a container
+export default connect(mapStateToProps)(BookList);
