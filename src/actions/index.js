e81ba92ff8b88199ae1bc79a84de852211802574
Dylan Ellison <dylancellison@gmail.com>
2018-04-24 23:31:33 -0500

    created action creator

---
+++ src/actions/index.js
@@ -0,0 +1,3 @@
+export function selectBook(book) {
+  console.log("A abook has been selected", book.title);
+}
--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import { connect } from 'react-redux';
+import { selectBook}from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 
 class BookList extends Component {
@@ -29,5 +31,19 @@ function mapStateToProps(state){
 
   };
 }
+// anything returned to this function will end up as props on the BooksList container
+function mapDispatchToProps(dispatch){
+  // whenever select book is called, the result
+  // should be passed to all of our reducers
+  // dispatch spreads all the actions to all the reducers in the app
+  return bindActionCreators({selectBook: selectBook}, dispatch)
+
+}
 // connect takes a function and a component and produces a container
-export default connect(mapStateToProps)(BookList);
+
+// Promote BookList from a component to a cointainer - it needs to know about
+// this new dispatch method, SelectedBook. Make it available as a props
+
+// this is probably the most confusing code in redux.
+// check react-redux doc. to understand this better. great guide.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
